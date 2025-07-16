feae59008c79a598bdff3d57650eaa86f71ce695
nguyenbalinh1401 <linhnbse161930@fpt.edu.vn>
2025-07-16 13:17:51 +0700

    config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,12 +8,12 @@ import prettier from 'eslint-config-prettier';
 import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  // ❌ Ignore folder/files
+
   {
     ignores: ['node_modules/**', 'dist/**', '.next/**', 'src/generated/**'],
   },
 
-  // ✅ Base JS setup
+
   {
     files: ['**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
@@ -25,12 +25,11 @@ export default defineConfig([
     extends: ['js/recommended'],
   },
 
-  // ✅ TypeScript support
+
   ...tseslint.configs.recommended,
 
-  // ✅ Next.js rules
   ...next(),
 
-  // ✅ Prettier integration (optional but recommended)
+
   prettier,
 ]);
